3cbbce4dbca97fd2493d857c2440fee4a5256848
Mariam <77506232+MariamBabai@users.noreply.github.com>
2021-09-18 14:07:03 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,25 +1,58 @@
-const EMAIL = document.getElementById("emailName");
-const NAME= document.getElementById("nameName");
-const SURNAME= document.getElementById("surName");
-const PASSWORD = document.getElementById("passwordName");
+h1
+{
+    margin: 4vw;
+    text-align: center;
+    font-family: "Comic Sans MS";
+}
+body
+{
+    background-color: aquamarine;
+    background-repeat: no-repeat;
+    background-position-x: center;
+    background-position-y: 130px;
+    background-size: 800px;
 
-
-const SBMTBUTTON = document.getElementById('sbmtBtn');
-
-function Form(email, name, surname, password) {
-    this.email = email;
-    this.name = name;
-    this.surname = surname;
-    this.password = password;
 
 }
-//<input type="submit" className="validateBtn" value="validate">
-//let validateBtn = document.querySelector(".validateBTN");
-//if
-SBMTBUTTON.addEventListener('submit', function ()
+form
 {
-    console.log("SUBMITTED")
+    text-align: center;
+    align-content: center;
+    justify-content: center;
 
-});
 
 
+}
+div label{
+    text-align: center;
+    align-content: center;
+    justify-content: center;
+    font-family: "Comic Sans MS";
+    margin: 2px;
+
+
+}
+input
+{
+    [type = text];
+    text-align: center;
+    justify-content: center;
+    background-color: azure;
+    border-style: none;
+    width: 20vw;
+    height: 2vw;
+
+}
+button {
+    margin-top: 10px;
+    font-size: 35px;
+    background-color: aquamarine ;
+    border-style: none;
+    font-family: "Comic Sans MS";
+
+}
+button:hover
+{
+    color: white;
+    cursor: pointer;
+}
